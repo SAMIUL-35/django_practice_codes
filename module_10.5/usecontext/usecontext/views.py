e7a81ad80097c45065246bc642f114e9b60dc424
SAMIUL2022 <sami35bd@gmail.com>
2024-10-06 21:38:50 +0600

    practice add,addslashes,carfirst,center,cut,date,default,discort,make list,random,slice,upper,lower,wordcount foltering from https://www.geeksforgeeks.org/ django template filters

--- module_10.5/usecontext/usecontext/views.py
+++ module_10.5/usecontext/usecontext/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from datetime import datetime
 def home(request):
    data={
       'name':'samiul',
       'roll':'45',
-      'salary':'120000'
+      'salary':'120000',
+      'lst':['s','a','d','k'],
+      'num':5023,
+      'value':'I’m Jai',
+      'datetime':datetime.now(),
+      'empty':'',
+     'name_list':[
+    {'name': 'zed', 'age': 19},
+    {'name': 'amy', 'age': 22},
+    {'name': 'joe', 'age': 31},
+],
    }
    return  render (request,'home.html',data)
