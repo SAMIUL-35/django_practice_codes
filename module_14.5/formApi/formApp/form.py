d50aea2e79e729e666eaefbc85e77285777d3df1
SAMIUL2022 <sami35bd@gmail.com>
2024-10-18 23:20:35 +0600

    practice Django Model Fields and Fieldsets

--- module_14.5/formApi/formApp/form.py
+++ module_14.5/formApi/formApp/form.py
@@ -2,7 +2,11 @@ from django import forms
 import datetime
 
 BIRTH_YEAR_CHOICES = [x for x in range(1900, datetime.datetime.now().year + 1)]
-
+FAVORITE_COLORS_CHOICES = [
+    ('blue', 'Blue'),
+    ('green', 'Green'),
+    ('black', 'Black'),
+]
 class ApForm(forms.Form):  
     name = forms.CharField(max_length=20, label='Enter Your Name')
     email = forms.EmailField(max_length=30, label='Enter Your Email')
@@ -11,3 +15,11 @@ class ApForm(forms.Form):
     value = forms.DecimalField(max_digits=10, decimal_places=2)
     comment = forms.CharField(widget=forms.Textarea(attrs={'rows': 1}), label='Enter Your Comment')
     agree = forms.BooleanField()
+    roll_number = forms.IntegerField( 
+                     help_text = "Enter 6 digit roll number"
+                     ) 
+    
+    first_name = forms.CharField(initial='Your name')
+    favorite_color = forms.ChoiceField(choices=FAVORITE_COLORS_CHOICES)
+    favorite_color = forms.ChoiceField(widget=forms.RadioSelect, choices=FAVORITE_COLORS_CHOICES)
+    favorite_colors = forms.MultipleChoiceField(choices=FAVORITE_COLORS_CHOICES)
--- module_14.5/formApi/formApp/models.py
+++ module_14.5/formApi/formApp/models.py
@@ -6,7 +6,18 @@ class Form(models.Model):
     email = models.EmailField(max_length=30)
     password = models.CharField(max_length=128)  
     birth_date = models.DateField(default=timezone.now)
-    value = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  
+    value = models.DecimalField(max_digits=10, decimal_places=2)  
+    auto_field = models.AutoField(primary_key=True)
+    
+    big_integer_field = models.BigIntegerField()
+    binary_field = models.BinaryField()
+    boolean_field = models.BooleanField()
+    duration_field = models.DurationField()
+    file_field = models.FileField(upload_to='files/')
+    ile_path_field = models.FilePathField(path='/path/to/files/')
+    image_field = models.ImageField(upload_to='images/')
+    slug_field = models.SlugField()
+
 
     def __str__(self):
         return self.name
